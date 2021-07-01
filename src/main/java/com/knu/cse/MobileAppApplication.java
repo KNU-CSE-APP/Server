eda7a6b688cbb21b41796d5cc37dd738e28f4e39
nowwater <6360879@naver.com>
2021-07-01 22:58:12 +0900

    ESTABLISH: project setup

---
+++ src/main/java/com/knu/cse/MobileAppApplication.java
@@ -0,0 +1,13 @@
+package com.knu.cse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppApplication.class, args);
+	}
+
+}
