d5f07004faee0ab76abed168386962509d53fbaf
nowwater <6360879@naver.com>
2021-08-03 18:02:31 +0900

    FIX: Board 도메인 수정

--- src/main/java/com/knu/cse/board/domain/Board.java
+++ src/main/java/com/knu/cse/board/domain/Board.java
@@ -7,6 +7,7 @@ import com.knu.cse.member.model.Member;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -16,13 +17,13 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@NoArgsConstructor @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @Getter
 public class Board extends BaseTimeEntity {
@@ -45,7 +46,7 @@ public class Board extends BaseTimeEntity {
     @JoinColumn(name="member_id")
     private Member member;
 
-    @Builder.Default @OneToMany(mappedBy="board")
+    @Builder.Default @OneToMany(mappedBy="board", cascade = CascadeType.REMOVE)
     private List<Comment> commentList = new ArrayList<Comment>();
 
     public void edit(BoardForm boardForm){
