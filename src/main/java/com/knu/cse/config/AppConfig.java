0b37c16221dd6c23c7898ed1eab745c23f9b1417
nowwater <6360879@naver.com>
2021-08-14 02:21:46 +0900

    FEAT: 작성한 사람, 수정한 사람 Auditing 추가

--- src/main/java/com/knu/cse/config/AppConfig.java
+++ src/main/java/com/knu/cse/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.knu.cse.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -11,5 +12,11 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    // CreatedBy 나 LastModifiedBy 에 사용자를 찾기 위해 이 메소드를 호출해서 결과물을 꺼내간다.
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new Auditor();
     }
 }
