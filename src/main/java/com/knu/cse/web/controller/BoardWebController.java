9b992375528c267e6679da40cffb35ab364b26b2
Chang Muk Kim <57346393+kcm0147@users.noreply.github.com>
2021-08-30 23:14:30 +0900

    [#93] FEAT: Admin 게시물 작성 및 게시물조회

---
+++ src/main/java/com/knu/cse/web/controller/BoardWebController.java
@@ -0,0 +1,51 @@
+package com.knu.cse.web.controller;
+
+import com.knu.cse.board.domain.Category;
+import com.knu.cse.board.dto.BoardDto;
+import com.knu.cse.board.dto.BoardForm;
+import com.knu.cse.board.service.BoardService;
+import com.knu.cse.email.service.AuthService;
+import com.knu.cse.errors.NotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin")
+public class BoardWebController {
+
+
+    private final BoardService boardService;
+
+    private final AuthService authService;
+
+    @GetMapping("/boardlist")
+    public String boardList(Model model){
+        List<BoardDto> BoardList = boardService.findByCategory(Category.ADMIN).stream()
+            .map(BoardDto::new).collect(Collectors.toList());
+        model.addAttribute("BoardMap",BoardList);
+
+        return "boardList";
+    }
+
+    @GetMapping("/board")
+    public String oneBoard(@ModelAttribute(name="boardForm") BoardForm boardForm){
+        return "write";
+    }
+
+    @PostMapping("/board")
+    public String writeBoard(@ModelAttribute(name="boardForm") BoardForm boardForm)
+        throws NotFoundException, IOException {
+        Long userId = authService.getUserIdFromJWT();
+        boardService.writeBoard(userId,boardForm);
+        return "redirect:/admin/boardlist";
+    }
+}
