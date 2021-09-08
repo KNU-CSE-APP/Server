20f62d367ce2254fe2f7c5ae4b2b5774d4e6473d
Girin7716 <k3832580@naver.com>
2021-09-09 01:07:41 +0900

    [#93] FEAT : 인증허가 되지 않은 유저 접근 제한

--- src/main/java/com/knu/cse/config/SecurityConfig.java
+++ src/main/java/com/knu/cse/config/SecurityConfig.java
@@ -1,36 +1,74 @@
 package com.knu.cse.config;
 
+import com.knu.cse.email.service.AuthService;
+import com.knu.cse.email.service.MemberDetailsService;
+import com.knu.cse.email.util.JwtRequestFilter;
+import com.knu.cse.email.util.JwtUtil;
+import com.knu.cse.member.model.Member;
+import com.knu.cse.member.model.MemberRole;
+import com.knu.cse.member.repository.MemberRepository;
+import com.knu.cse.member.security.SecurityMember;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Optional;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final JwtRequestFilter jwtRequestFilter;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        /*
+        참고 사이트
+        https://oingdaddy.tistory.com/206
+        https://qodbtn.tistory.com/307
+        https://fenderist.tistory.com/344
+         */
         http.authorizeRequests()
-            .mvcMatchers("/**", "/swagger-ui.html", "/user/**").permitAll()
-            .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
-            .anyRequest().authenticated();
+                .mvcMatchers("/","/home","/swagger-ui.html","/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/user/**").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
+                .anyRequest().authenticated()
+                .and().exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
+            @Override
+            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                response.sendRedirect("/home");
+            }
+        });
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
         http.cors().and();
         http.csrf().disable();
-        //http.formLogin()
-        //    .loginPage("/signIn").permitAll();
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
-            .mvcMatchers("/node_modules/**")
-            .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+                .mvcMatchers("/node_modules/**")
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
+
 }
