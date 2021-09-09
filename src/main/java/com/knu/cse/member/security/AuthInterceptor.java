11287893d7d646fc09a2263fcd317e0f43f86421
Chang Muk Kim <57346393+kcm0147@users.noreply.github.com>
2021-09-10 01:40:45 +0900

    [#93] FEAT: board 관련 컨트롤러에 관리자권한 확인하는 인터셉터 추가

---
+++ src/main/java/com/knu/cse/member/security/AuthInterceptor.java
@@ -0,0 +1,72 @@
+package com.knu.cse.member.security;
+
+import com.knu.cse.email.service.AuthService;
+import com.knu.cse.errors.NotFoundException;
+import com.knu.cse.member.model.Member;
+import com.knu.cse.member.model.MemberRole;
+import com.knu.cse.member.repository.MemberRepository;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+
+@RequiredArgsConstructor
+@Component
+public class AuthInterceptor implements HandlerInterceptor {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+        Object handler) throws Exception {
+
+        if( handler instanceof HandlerMethod == false ) {
+            return true;
+        }
+        HandlerMethod handlerMethod = (HandlerMethod)handler;
+
+        // @Auth 받아오기
+        Auth auth = handlerMethod.getMethodAnnotation(Auth.class);
+        Auth adminRole = handlerMethod.getMethod().getDeclaringClass().getAnnotation(Auth.class);
+
+        if( auth == null && adminRole == null) {
+            return true;
+        }
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if( authentication.getPrincipal().equals("anonymousUser") ) {
+            response.sendRedirect(request.getContextPath() + "/home/");
+            return false;
+        }
+
+        SecurityMember securityMember = (SecurityMember) authentication.getPrincipal();
+
+        String email = securityMember.getEmail();
+        if ( email == null ) {
+            response.sendRedirect(request.getContextPath() + "/home/");
+            return false;
+        }
+
+        Member member = memberRepository.findByEmail(email).orElseThrow(() ->
+            new NotFoundException("존재하지 않는 회원입니다."));
+
+        String role = adminRole.role().toString();
+        if(MemberRole.ROLE_ADMIN.equals(role) ) {
+            if(MemberRole.ROLE_ADMIN.equals(member.getRole())){
+                response.sendRedirect(request.getContextPath() + "/home/");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
